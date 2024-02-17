a3a7cd882e72a19c0bede3b3c413a107a706c95f
Perarulalan V <95120766+perarulalan15@users.noreply.github.com>
2024-02-17 21:21:57 +0530

    Update settings.py

--- horse_health/settings.py
+++ horse_health/settings.py
@@ -127,3 +127,8 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://horse-health-django.up.railway.app'
+]
